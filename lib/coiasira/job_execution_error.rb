55ef51be55a3c3a2149b0fba1a13dab21971f5f2
Brian Olsen <brian@maven-group.org>
2008-10-23 11:34:40 +0000

    Renamed bin/quartz to bin/coiasira and changed name of one of the arguments for JobExecutionError.

--- lib/coiasira/job_execution_error.rb
+++ lib/coiasira/job_execution_error.rb
@@ -1,11 +1,11 @@
 module Coiasira
   class JobExecutionError < SchedulerError
-    attr_accessor :refire, :unschedule_trigger, :unchedule_all_triggers
+    attr_accessor :refire, :unschedule_firing_trigger, :unchedule_all_triggers
     
     def initialize(options={})
-      options.assert_valid_keys(:refire_immediately, :unschedule_trigger, :unchedule_all_triggers)
+      options.assert_valid_keys(:refire_immediately, :unschedule_firing_trigger, :unchedule_all_triggers)
       @refire = options[:refire_immediately] || false
-      @unschedule_trigger = options[:unschedule_trigger] || false
+      @unschedule_trigger = options[:unschedule_firing_trigger] || false
       @unchedule_all_triggers = options[:unchedule_all_triggers] || false
     end
   end
